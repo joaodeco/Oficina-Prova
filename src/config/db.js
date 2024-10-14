a890a511a8cf7ea1f4677567ddc14cc8b18bc22c
joaodeco <murilomergner@gmail.com>
2024-10-14 17:14:45 -0300

    SLslslsls

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     mongoose
-      .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${CLUSTER_ADDRES}`)
+      .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRES}`)
       .then(() => console.log("Conectado ao MongoDB"))
       .catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
 };
